b230f386784f2a0f1f5f23b18f3abae325e4cbb4
Michał Niegrzybowski <michal.niegrzybowski@gmail.com>
2019-11-29 12:53:47 +0100

    make it .net core 3.0 compliant

--- Program.fs
+++ Program.fs
@@ -2,17 +2,30 @@
     module Main =
         open System.IO
         open Microsoft.AspNetCore.Hosting
+        open Microsoft.Extensions.Hosting
+        open Microsoft.Extensions.Logging
         open NancyCore.Web.Config
 
 
         [<EntryPoint>]
-        let main _ =
+        let main argv =
             let host = 
-                WebHostBuilder()
-                    .UseContentRoot(Directory.GetCurrentDirectory())
-                    .UseKestrel()
-                    .UseStartup<Startup>()
+                Host
+                    .CreateDefaultBuilder(argv)
+                    .ConfigureWebHostDefaults(fun wb -> 
+                        wb
+                            .UseKestrel(fun k -> 
+                                k.AllowSynchronousIO <- true
+                            )
+                            .UseContentRoot(Directory.GetCurrentDirectory())
+                            .UseStartup<Startup>()
+                            .ConfigureLogging(fun _ lg -> 
+                                lg
+                                    .AddDebug()
+                                    .AddConsole() |> ignore
+                            ) |> ignore
+                        ()
+                    )
                     .Build()
-
             host.Run()
             0
